e466940ece9ed28762911d3c759a7ed4b0bb9231
Stormacq, Sebastien <stormacq@amazon.lu>
2019-01-04 11:18:37 +0100

    simple lambda handler

--- lambda-function/Sources/hello/main.swift
+++ lambda-function/Sources/hello/main.swift
@@ -11,29 +11,7 @@ Log.logger = logger
 #endif
 
 func handler(context: Context, event: LambdaEvent) throws -> LambdaResponse {
-    Log.debug("from handler")
-
-    let url = URL(string: "https://www.amazon.com")!
-    
-    let semaphore = DispatchSemaphore(value: 0)
-    
-    print("Connecting to \(url)")
-    let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
-        if let e = error {
-            print("\(e)")
-        } else {
-            guard let resp = response as! HTTPURLResponse? else { return print("A")}
-            print("Response = \(resp.statusCode)")
-            guard let data = data else { return print("B") }
-            print("Received data = \(data)")
-            print(String(data: data, encoding: .utf8)!)
-        }
-        semaphore.signal()
-    }
-    
-    task.resume()
-    _ = semaphore.wait(timeout: .distantFuture)
-
+    Log.debug("lambda handler")
 
     return [
         "result": event["key1"] ?? "unknown key : key1"
