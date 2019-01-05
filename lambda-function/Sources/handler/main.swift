94dd096be98bb770dbe377341f8ff21999bdebd6
Stormacq, Sebastien <stormacq@amazon.lu>
2019-01-05 20:17:45 +0000

    simplified handler function

--- lambda-function/Sources/handler/main.swift
+++ lambda-function/Sources/handler/main.swift
@@ -13,31 +13,39 @@ import LoggerAPI
 func handler(context: Context, event: LambdaEvent) throws -> LambdaResponse {
     Log.debug("Starting lambda handler")
 
-    // fetch data from https://httpbin.org/json
-    let endpoint = "https://httpbin.org/get?value=\(event["key1"] ?? "no value provided")"
-    Log.debug(endpoint)
-    let (data, response, error) = URLSession.shared.synchronousDataTask(with: endpoint) //if url is invalid, it is a programming error
-    
-    // did we receive an error ?
-    guard error == nil else {
-        return  [ "error": error!.localizedDescription ]
-    }
-    
-    guard let event = data else {
-        return  [ "error": "no body returned" ]
-    }
-    
-    guard let resp = response else {
-        return  [ "error": "no response returned" ]
-    }
-    
-    if (resp.statusCode == 200) {
-        return try JSONify(jsonData: event) as LambdaEvent
-    } else {
-        return  [ "error": String(resp.statusCode) ]
-    }
+    return [ "result": event["key1"] ?? "no key1 provided" ]
 
 }
 
 try LambdaRuntime(handler).run()
 
+// idea for a v2
+
+// func handler(context: Context, event: LambdaEvent) throws -> LambdaResponse {
+//     Log.debug("Starting lambda handler")
+
+//     // fetch data from https://httpbin.org/json
+//     let endpoint = "https://httpbin.org/get?value=\(event["key1"] ?? "no value provided")"
+//     Log.debug(endpoint)
+//     let (data, response, error) = URLSession.shared.synchronousDataTask(with: endpoint) //if url is invalid, it is a programming error
+    
+//     // did we receive an error ?
+//     guard error == nil else {
+//         return  [ "error": error!.localizedDescription ]
+//     }
+    
+//     guard let event = data else {
+//         return  [ "error": "no body returned" ]
+//     }
+    
+//     guard let resp = response else {
+//         return  [ "error": "no response returned" ]
+//     }
+    
+//     if (resp.statusCode == 200) {
+//         return try JSONify(jsonData: event) as LambdaEvent
+//     } else {
+//         return  [ "error": String(resp.statusCode) ]
+//     }
+
+// }
