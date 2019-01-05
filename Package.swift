5542709f68482f4c0bca35e165bb3710b49f51dc
Stormacq, Sebastien <stormacq@amazon.lu>
2019-01-05 17:39:57 +0000

    change executable name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "hello",
+    name: "HelloSwiftLambda",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -20,7 +20,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "hello",
+            name: "HelloSwiftLambda",
             dependencies: ["LambdaRuntime", "HeliumLogger"],
             path: "lambda-function/Sources"
         ),
@@ -30,8 +30,8 @@ let package = Package(
             path: "lambda-runtime/Sources"
         ),
         .testTarget(
-            name: "helloTests",
-            dependencies: ["hello"],
+            name: "HelloSwiftLambdaTests",
+            dependencies: ["HelloSwiftLambda"],
             path: "lambda-function/Tests"
         ),
     ]
